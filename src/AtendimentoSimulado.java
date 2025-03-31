7faf0db824ea58595ff9860c4fbc4a8f8ccbc1cd
gabrielmallet <bielmallet@hotmail.com>
2025-03-31 19:01:38 -0300

    Commit 4

--- src/AtendimentoSimulado.java
+++ src/AtendimentoSimulado.java
@@ -7,18 +7,21 @@ public class AtendimentoSimulado { ;
     public void processo(JTextArea areatexto){
         try{
             areatexto.append("\n"+ Thread.currentThread().getName() + " esperando atendimento...\n");
-            Thread.sleep(1000);
 
+            Thread.sleep(1000);
             lock.lock();
             if(Thread.currentThread().isInterrupted()){
                 return;
             }
+
             areatexto.append("\n"+ Thread.currentThread().getName() + " sendo atendido...\n");
             long tempoInicio = System.currentTimeMillis();
             Thread.sleep((long)(Math. random() * 5000));
+
             long tempoFim = System.currentTimeMillis();
             long tempoTotalAtendimento = tempoFim - tempoInicio;
             areatexto.append(Thread.currentThread().getName() + " Atendimento Finalizado! (Atendimento durou " + tempoTotalAtendimento + " ms)\n");
+
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             areatexto.append("\n"+ Thread.currentThread().getName() + " ATENDIMENTO INTERROMPIDO!\n");
