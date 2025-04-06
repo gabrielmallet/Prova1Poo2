3f7ee0f200e88ba53e55095340a053060e1e2db0
gabrielmallet <bielmallet@hotmail.com>
2025-04-06 17:01:30 -0300

    Commit 6

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,13 +1,9 @@
 import javax.swing.*;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class Cliente extends Thread{
     private int numeroCliente;
     private JTextArea areatexto;
     private AtendimentoSimulado atendimentoSimulado;
-    private final ReentrantLock lock = new ReentrantLock();
-
-
 
     public Cliente(JTextArea areatexto, AtendimentoSimulado atendimentoSimulado, int numeroCliente){
         this.areatexto = areatexto;
